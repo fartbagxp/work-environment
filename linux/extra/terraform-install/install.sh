186c6ebd308ed750a3f5dbacffb7c88acba9e502
Boris Ning <fartbagxp@gmail.com>
2019-09-05 20:52:29 -0400

    Modifying terraform to have version information

--- linux/extra/terraform-install/install.sh
+++ linux/extra/terraform-install/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-unzip terraform_0.11.7_linux_amd64.zip
+version=0.12.8
+wget https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip
+unzip terraform_${version}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
