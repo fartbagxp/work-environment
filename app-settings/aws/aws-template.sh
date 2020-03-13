c5ec982acd129b60331ba15d389b91d538675dbb
fartbagxp <fartbagxp@gmail.com>
2020-03-13 11:06:30 -0400

    Updating aws-template

--- app-settings/aws/aws-template.sh
+++ app-settings/aws/aws-template.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Switching to templated AWS environment!"
-export AWS_PROFILE=<should match ~/.aws/config>
-export AWS_ACCESS_KEY_ID=
-export AWS_SECRET_ACCESS_KEY=
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "${bold}ERROR:${normal} Check that you are properly sourcing the script"
+    echo
+    echo "This script should be run as:"
+    echo "$ ${bold}source${normal} ./aws-fartbagxp.sh"
+    exit 1
+fi
+
+echo "Switching AWS environment!"
+export AWS_PROFILE=default
+export AWS_ACCESS_KEY_ID=123456
+export AWS_SECRET_ACCESS_KEY=123456
 export AWS_DEFAULT_REGION=us-east-1
+
+rm -rf ~/.aws/credentials
+
+aws configure set aws_access_key_id 123456
+aws configure set aws_secret_access_key 123456
+aws configure set default.region us-east-1
+aws configure set default.output json
